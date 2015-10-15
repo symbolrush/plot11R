983c7e1bd60833b768518ed1b98502321e580675
Adrian Schmid <trompetenschmid@gmail.com>
2015-10-15 19:45:37 +0200

    Tests für cpDtService() und filterPrio12()

--- tests/testthat/test_helpers.R
+++ tests/testthat/test_helpers.R
@@ -1,3 +1,19 @@
 test_that("fhsblue() works", {
   expect_equal(fhsblue(), "#006699")
 })
+
+test_that("cpDtService() works", {
+  expect_equal(cpDtService(90), 'green')
+  expect_equal(cpDtService(80), 'yellow')
+  expect_equal(cpDtService(70), 'orange')
+  expect_equal(cpDtService(60), 'red')
+  expect_equal(cpDtService(NA), 'blue')
+})
+
+test_that("filterPrio12() works", {
+  simEvents <- data.frame(
+    id = c(1,2,3,4),
+    priority = c(3,1,2,4)
+  )
+  expect_equal(nrow(filterPrio12(simEvents)), 2)
+})
