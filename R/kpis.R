0b8c33b15210f4ff54c267ab88d61eb3b5fa474d
Adrian Schmid <trompetenschmid@gmail.com>
2015-10-15 19:44:21 +0200

    Verschiedene Bugs und Weiterentwicklungen: 1. kpiEventsInTimeDelta(): Filterung nicht mehr in der Funktion 2. kpiEventsNewInTime(): Greift nun auf tableEventsNewInTime() zu zur Berechnung. 3. kpiEventsNewNotInTime(): Greift nun auf tableEventsNewInTime() zu zur Berechnung. 4. kpiMissionsExternalServiceNeeded(): neu geschrieben. 5. kpiMissionsByOrganisation(): neu geschrieben 6. kpiEventsInTimeByOrganisationPrio12(): neu geschrieben. 7. kpiEventsByOrganisationPrio12(): neu geschrieben. 8. kpiEventsFaster(): neu geschrieben. 9. kpiEventsFasterPercentage(): neu geschrieben.

--- R/kpis.R
+++ R/kpis.R
@@ -6,7 +6,6 @@
 #'
 #' @return EventsInTime A percentage of the fraction of the events in time
 kpiEventsInTime <- function(simEvents) {
-  simEvents <- simEvents[simEvents$priority %in% c(1,2), ]
   return(
     round(
       nrow(simEvents[simEvents$dtService < 15*60,])/nrow(simEvents)*100, 1))
@@ -20,26 +19,67 @@ kpiEventsInTime <- function(simEvents) {
 #'
 #' @return EventsInTimeDelta A numeric
 kpiEventsInTimeDelta <- function(simEvents, simEventsBefore) {
-  simEvents0 <- simEventsBefore[simEventsBefore$priority %in% c(1,2), ]
-  simEvents1 <- simEvents[simEvents$priority %in% c(1,2), ]
-  percentage1 <- nrow(
-    simEvents1[simEvents1$dtService < 15*60,])/nrow(simEvents1)*100
-  percentage0 <- nrow(
-    simEvents0[simEvents0$dtService < 15*60,])/nrow(simEvents0)*100
-  round(percentage1 - percentage0, 1)
+  percentage <- nrow(simEvents[simEvents$dtService < 15*60,])/nrow(simEvents)*100
+  percentageBefore <- nrow(
+    simEventsBefore[simEventsBefore$dtService < 15*60,])/nrow(simEventsBefore)*100
+  round(percentage - percentageBefore, 1)
 }
 
 kpiEventsNewInTime <- function(simEvents, simEventsBefore) {
-#   simEvents0 <- simEventsBefore[simEventsBefore$priority %in% c(1,2), ]
-#   simEvents1 <- simEvents[simEvents$priority %in% c(1,2), ]
-#   simEvents1 <- simEvents1[simEvents1$dtService < simEvents0$dtService, ]
-#   simEvents1 <- simEvents1[simEvents1$dtService < 15*60, ]
-  return("Bullshit")
+  nrow(plot11R::tableEventsNewInTime(simEvents, simEventsBefore))
 }
 
 kpiEventsNewNotInTime <- function(simEvents, simEventsBefore) {
-  simEvents0 <- simEventsBefore[simEventsBefore$priority %in% c(1,2), ]
-  simEvents1 <- simEvents[simEvents$priority %in% c(1,2), ]
-  simEvents1 <- simEvents1[simEvents1$dtService > simEvents0$dtService, ]
-  simEvents1 <- simEvents1[simEvents1$dtService >= 15*60, ]
+  nrow(plot11R::tableEventsNewNotInTime(simEvents, simEventsBefore))
+}
+
+kpiMissionsExternalServiceNeeded <- function(simMissions) {
+  length(simMissions$vehicleId[simMissions$vehicleId %in% c(1000,2000)])
+}
+
+kpiMissionsByOrganisation <- function(simScenario, organisation) {
+  vehicleId <- simScenario$vehicles$id[simScenario$vehicles$organisation == organisation]
+  length(simScenario$missions$vehicleId[simScenario$missions$vehicleId %in% vehicleId])
+}
+
+kpiEventsInTimeByOrganisationPrio12 <- function(simScenario, organisation) {
+  vehicleId <- simScenario$vehicles$id[simScenario$vehicles$organisation == organisation]
+  eventId <- unique(simScenario$missions$eventId[simScenario$missions$vehicleId %in% vehicleId])
+  simEvents <- simScenario$events[simScenario$events$id %in% eventId, ]
+  simEvents <- filterPrio12(simEvents)
+  nrow(simEvents[simEvents$dtService < 15*60, ])/nrow(simEvents)*100
+}
+
+kpiEventsByOrganisationPrio12 <- function(simScenario, organisation) {
+  vehicleId <- simScenario$vehicles$id[simScenario$vehicles$organisation == organisation]
+  eventId <- unique(simScenario$missions$eventId[simScenario$missions$vehicleId %in% vehicleId])
+  simEvents <- simScenario$events[simScenario$events$id %in% eventId, ]
+  simEvents <- filterPrio12(simEvents)
+  nrow(simEvents)
+}
+
+#' kpiEventsFaster(): Anzahl der Events, welche schneller als im
+#' zugrundeliegenden Referenzszenario erreicht wurden.
+#'
+#' @param simEvents
+#' @param simEventsRef
+#'
+#' @return
+#' @export
+#'
+#' @examples
+kpiEventsFaster <- function(simEvents, simEventsRef) {
+  nrow(tableEventsNewFaster(simEvents, simEventsRef)) -
+    nrow(tableEventsNewSlower(simEvents, simEventsRef))
+}
+
+#' kpiEventsFasterPercentage(): Prozentsatz der Events, welche schneller als
+#' im zugrundeliegenden Referenzszenario erreicht wurden.
+#'
+#' @param simEvents
+#' @param simEventsRef
+#'
+#' @return kpiEventsFasterPercentage A number
+kpiEventsFasterPercentage <- function(simEvents, simEventsRef) {
+  round(kpiEventsFaster(simEvents, simEventsRef)/nrow(simEvents)*100, 1)
 }
