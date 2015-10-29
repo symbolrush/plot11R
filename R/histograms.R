b363308b0941c24920f224746b9080504ca3f4e5
Adrian Schmid <trompetenschmid@gmail.com>
2015-10-29 11:53:09 +0100

    Histograms:
    1. histDtService(): funktioniert jetzt mit simEvents und simMissions
    2. histTAlarmHour(): geschrieben und dokumentiert
    3. histTAlarmDay(): geschrieben und dokumentiert
    4. histTAlarmWeekday(): geschrieben und dokumentiert
    5. histTAlarmMonth(): geschrieben und dokumentiert. Achtung diese Funktion verwendet noch eine etwas unschöne Näherung! (Die Zuordnung des Monats passiert mit der Division durch die Zeit und dann durch 30.5 Tage.

--- R/histograms.R
+++ R/histograms.R
@@ -1,12 +1,20 @@
 #' histDtService: that famous (but not very useful) histogram of all events.
 #'
-#' @param simEvents A set of simEvents
+#' @param simData A set of simEvents or simMissions
 #'
 #' @return plotted histogram
-histDtService <- function(simEvents) {
-  dtService <- simEvents$dtService/60
+histDtService <- function(simData) {
+  if ("simMissions" %in% class(simData)) {
+    dtService <- simData$dtLaunch + simData$dtToPoA
+  } else {
+    dtService <- simData$dtService
+  }
+  # dtService in Minutes
+  dtService <- dtService/60
+  # Only look at simData with dtService between 0 and 60 min.
   dtService <- dtService[dtService >= 0]
   dtService <- dtService[dtService < 60]
+  # plot it
   hist(
     dtService,
     breaks = 60,
@@ -18,4 +26,87 @@ histDtService <- function(simEvents) {
   )
 }
 
+
+
+
+#' histTAlarmHour:
+#'
+#' @param simEvents A set of simMissions
+#'
+#' @return plotted histogram
+histTAlarmHour <- function(simMissions) {
+  tAlarmHour <- plyr::count(trunc(simTimeR::simTime(simMissions$tAlarm)/60/60))
+  barplot(
+    tAlarmHour$freq,
+    main = "Einsatzverteilung über den Tag",
+    ylab = "Anzahl Einsätze",
+    xlab = "Stunde",
+    col = fhsblue(),
+    names.arg = c(0:23)
+  )
+}
+
+
+#' histTAlarmDay:
+#'
+#' @param simEvents A set of simMissions
+#'
+#' @return plotted histogram
+histTAlarmDay <- function(simMissions) {
+  tAlarmDay <- plyr::count(simTimeR::simDate(simMissions$tAlarm))
+  barplot(
+    tAlarmDay$freq,
+    main = "Einsatzverteilung über das Jahr, nach Tag aufgelöst",
+    ylab = "Anzahl Einsätze",
+    xlab = "Tag",
+    col = fhsblue()
+  )
+}
+
+#' histTAlarmWeekday:
+#'
+#' @param simMissions A set of simMissions
+#'
+#' @return plotted histogram
+histTAlarmWeekday <- function(simMissions) {
+  t <- plyr::count(simTimeR::simWeekday(simMissions$tAlarm))
+  barplot(
+    c(
+      t$freq[t$x == "Mo"],
+      t$freq[t$x == "Di"],
+      t$freq[t$x == "Mi"],
+      t$freq[t$x == "Do"],
+      t$freq[t$x == "Fr"],
+      t$freq[t$x == "Sa"],
+      t$freq[t$x == "So"]
+      ),
+    main = "Einsatzverteilung nach Wochentag",
+    ylab = "Anzahl Einsätze",
+    xlab = "Tag",
+    col = fhsblue(),
+    names.arg = c("Mo", "Di", "Mi", "Do", "Fr", "Sa", "So")
+  )
+}
+
+
+#' histTAlarmMonth:
+#'
+#' @param simMissions A set of simMissions
+#'
+#' @return plotted histogram
+histTAlarmMonth <- function(simMissions) {
+  hist(
+    simMissions$tAlarm/60/60/24/30.5,
+    axes = FALSE,
+    breaks = 12,
+    xlab = "Monat",
+    ylab = "Anzahl Einsätze",
+    main = "Einsatzverteilung über das Jahr, nach Monat aufgelöst",
+    col = fhsblue())
+  axis(1,
+       at = c(0.5:11.5),
+       labels = c("Jan","Feb","Mar","Apr","Mai","Jun",
+                  "Jul","Aug","Sep","Okt","Nov","Dez"))
+  axis(2)
+}
 
