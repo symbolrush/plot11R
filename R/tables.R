ea570c69c957c3f3ff86dd64dda96b5ea93fb831
Adrian Schmid <trompetenschmid@gmail.com>
2015-10-29 11:55:17 +0100

    Tabellen:
    1. tableDtService(): Liefert eine Übersicht über die wichtigsten Spalten zweier Sets an simScenarien. Zum Vergleich der Missions.
    2. tableDtServicePrio12(): Spezialfall von tableDtService(), verglichen werden nur die Missions mit Prio1 und 2.
    
    Beide Funktionen geschrieben und dokumentiert

--- R/tables.R
+++ R/tables.R
@@ -1,6 +1,43 @@
-tableEventsDifferenceDtService <- function(simEvents, simEventsBefore) {
-  return(simEvents[simEvents$dtService != simEventsBefore$dtService, ])
+#' tableDtService(): Liefert eine Tabelle mit ausgesuchten Spalten zweier Sets
+#' von simMissions
+#'
+#' @param simMissions A set of simMissions
+#' @param simMissionsBefore A set of simMissions
+#'
+#' @return tableDtService A data.frame
+tableDtService <- function(simScenario, simScenarioRef) {
+  simMissions <- simScenario$missions
+  simMissionsBefore <- simScenarioRef$missions
+  missions <- simMissions[
+    simMissions$dtToPoA != simMissionsBefore$dtToPoA, ]
+  missionsBefore <- simMissionsBefore[
+    simMissionsBefore$dtToPoA != simMissions$dtToPoA, ]
+  return(data.frame(
+    id = missions$id,
+    vehicleIdBefore = missionsBefore$vehicleId,
+    vehicleNameBefore = simScenarioRef$vehicles$name[missionsBefore$vehicleId],
+    vehicleId = missions$vehicleId,
+    vehicleName = simScenario$vehicles$name[missions$vehicleId],
+    dtToPoABefore = missionsBefore$dtToPoA,
+    dtToPoA = missions$dtToPoA,
+    lat = missions$lat,
+    lng = missions$lng
+  ))
 }
+
+#' tableDtServicePrio12
+#'
+#' @param simScenario
+#' @param simScenarioRef
+#'
+#' @return tableDtServicePrio12 A data.frame
+tableDtServicePrio12 <- function(simScenario, simScenarioRef){
+  simScenario$missions <- filterPrio12(simScenario$missions)
+  simScenarioRef$missions <- filterPrio12(simScenarioRef$missions)
+  tableDtService(simScenario, simScenarioRef)
+}
+
+
 
 tableEventsNewInTime <- function(simEvents, simEventsBefore) {
   inTime <- ifelse(simEvents$dtService < 15*60, TRUE, FALSE)
