ab0cd0e25c00e0fadfcff795a4e22cc9f720cfef
Adrian Schmid <trompetenschmid@gmail.com>
2015-10-30 14:02:11 +0100

    Pseudo Test für boxDtLaunchByOrganisation()

--- tests/testthat/test_boxplots.R
+++ tests/testthat/test_boxplots.R
@@ -2,3 +2,8 @@ test_that("boxDtLaunchByVehicleName() works", {
   load("../../data/simScenario.RData")
   expect_equal(boxDtLaunchByVehicleName(simScenario), "OK")
 })
+
+test_that("boxDtLaunchByOrganisation() works", {
+  load("../../data/simScenario.RData")
+  expect_equal(boxDtLaunchByOrganisation(simScenario), "OK")
+})
