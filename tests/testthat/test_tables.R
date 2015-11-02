3a1c1bcdf3f6cf289836cea48aa5afc05945197a
Adrian Schmid <trompetenschmid@gmail.com>
2015-11-02 17:46:01 +0100

    Begonnener Test für tableNrOfMissionsByOrganisation()

--- tests/testthat/test_tables.R
+++ tests/testthat/test_tables.R
@@ -4,3 +4,14 @@
 #   expect_equal(
 #     nrow(tableDtService(simScenario, simScenarioRef)), 5047)
 # })
+
+test_that("tableNrOfMissionsByOrganisationU() works", {
+  load("../../data/simScenario.RData")
+  load("../../data/simScenarioRef.RData")
+  simScenario$missions <- simScenario$missions[1:40,]
+  simScenarioRef$missions <- simScenarioRef$missions[1:40,]
+#   simScenario$missions <- simScenario$missions
+#   simScenarioRef$missions <- simScenarioRef$missions
+  table <- tableNrOfMissionsByOrganisation(simScenario,simScenarioRef)
+  print(table)
+})
